95d53602bb3b54b11900022618fe33135c0673b3
Tsierenana Bôtramanagna Gracy <gtsierenana@gmail.com>
2022-09-17 07:39:52 +0300

    Rename project from tz_wcc2_s2 to tz_wcc2_s3

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tz_wcc2_s2.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tz_wcc2_s3.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- tz_wcc2_s3/urls.py
+++ tz_wcc2_s3/urls.py
@@ -1,4 +1,4 @@
-"""tz_wcc2_s2 URL Configuration
+"""tz_wcc2_s3 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
