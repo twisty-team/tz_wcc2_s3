70994c670804323bd4ba53db6c1ad290fddffafb
Tsierenana Bôtramanagna Gracy <gtsierenana@gmail.com>
2022-09-18 10:46:10 +0300

    Add exchange creation mutation query

--- api_andao_atakalo/schema/mutations.py
+++ api_andao_atakalo/schema/mutations.py
@@ -1,16 +1,39 @@
-from graphene_django import DjangoObjectType
 import graphene
+from graphene_django import DjangoObjectType
+from graphene_file_upload.scalars import Upload
 
 from ..types import ExchangeType, PictureType, OwnerType
-from ..models import Exchange
+from ..models import Exchange, Owner
 
-# class CreateExchangeMutation(graphene.Mutation):
-#     pass
+
+class CreateExchangeMutation(graphene.Mutation):
+    class Arguments:
+        user_name = graphene.String()
+        contact = graphene.String()
+        toy_to_change = graphene.String()
+        desired_toy = graphene.String()
+        pictures = Upload(required=True)
+
+    exchange = graphene.Field(ExchangeType)
+    success = graphene.Boolean()
+
+    def mutate(self, info, user_name, contact, toy_to_change, desired_toy, pictures):
+        owner = Owner.objects.create(name=user_name, contact=contact)
+        exchange = Exchange.objects.create(
+            owner=owner,
+            toy_to_change=toy_to_change,
+            desired_toy=desired_toy,
+            # pictures=pictures
+        )
+        # TODO : File upload
+        # exchange.pictures = pictures.set()
+        return CreateExchangeMutation(exchange=exchange)
+
 
 class DeactivateExchangeMutation(graphene.Mutation):
     class Arguments:
         id = graphene.ID()
-    
+
     message = graphene.String()
     exchange = graphene.Field(ExchangeType)
 
@@ -19,11 +42,12 @@ class DeactivateExchangeMutation(graphene.Mutation):
 
     @classmethod
     def mutate(cls, root, info, id):
-        exchange = Exchange.objects.get(pk=id)
-        exchange.active = False 
+        exchange = Exchange.objects.get(id=id)
+        exchange.active = False
         exchange.save()
         return DeactivateExchangeMutation(exchange=exchange)
 
+
 class Mutation(graphene.ObjectType):
-    create_exchange = None
+    create_exchange = CreateExchangeMutation.Field()
     deactivate_exchange = DeactivateExchangeMutation.Field()
